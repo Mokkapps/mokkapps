094930189820c160fa448bc779c66ca51bb0397a
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2020-07-13 17:09:42 +0200

    Blog post test

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const parser = new Parser();
   const feed = await parser.parseURL("https://www.mokkapps.de/rss.xml");
   console.log(feed.title);
 
+  let result = md.render("# WIP!");
+
   feed.items.forEach((item) => {
+    result = md.render("# Latest Blog Posts");
+    result = md.render("- item.title + ":" + item.link");
     console.log(item.title + ":" + item.link);
   });
 
-  const result = md.render("# WIP!");
 
   fs.writeFile("README.md", result, function (err) {
     if (err) return console.log(err);
