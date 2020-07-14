c51304ef9cadb2c52ddf2a8cd38be3be375127f1
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2020-07-14 07:07:50 +0200

    Show latest blog posts

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-const md = require("markdown-it")();
+const md = require("markdown-it")({
+  breaks: true, // Convert '\n' in paragraphs into <br>
+  linkify: true, // Autoconvert URL-like text to links
+});
 const fs = require("fs");
 const Parser = require("rss-parser");
 
 const parser = new Parser();
 
+const feedUrl = "https://www.mokkapps.de/rss.xml";
+const websiteUrl = "https://www.mokkapps.de";
+const blogPostLimit = 5;
+
 (async () => {
-  const feed = await parser.parseURL("https://www.mokkapps.de/rss.xml");
-  console.log(feed.title);
+  let text = "";
+  try {
+    text = await loadBlogPosts(text);
+  } catch (e) {
+    console.error(`Failed to load blog posts from ${websiteUrl}`, e);
+  }
 
-  let text = "# Latest Blog Posts &nbsp;"
-
-  feed.items.forEach((item) => {
-    text += `- ${item.title} : ${item.link} &nbsp;`;
-  });
-
-  const result = md.render("# WIP");
+  const result = md.render(text);
 
   fs.writeFile("README.md", result, function (err) {
     if (err) return console.log(err);
     console.log(`${result} > README.md`);
   });
 })();
+
+async function loadBlogPosts(text) {
+  const feed = await parser.parseURL(feedUrl);
+
+  text = "# Latest Blog Posts \n";
+
+  feed.items.slice(0, blogPostLimit).forEach((item) => {
+    text += `- ${item.title} : ${item.link} \n`;
+  });
+
+  text += `\n More blog posts at ${websiteUrl}`;
+  return text;
+}
